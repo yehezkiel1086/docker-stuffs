d2034bc017af219ddd84ed238e011c095ba1c1cb
yehezkiel1086 <yehezkielwiradhika@gmail.com>
2025-02-18 12:59:05 +0700

    feat(cmds): add download script to start-conditions/yt-dlp/run.sh

--- cmds/start-conditions/yt-dlp/run.sh
+++ cmds/start-conditions/yt-dlp/run.sh
@@ -4,4 +4,10 @@
 docker build . -t yt-dlp
 
 # run container
-docker run yt-dlp
+docker run --name yt-dlp yt-dlp
+
+# check container diff
+docker diff yt-dlp
+
+# copy video from docker container to local machine (temporary container storage fix)
+docker cp "yt-dlp:/my-workspace/Master’s Programme in Computer Science ｜ University of Helsinki [XsqlHHTGQrw].mp4" .
