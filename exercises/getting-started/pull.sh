fc9537fb298a19c13d644a88e6288440c5ab1e1a
yehezkiel1086 <tsukikagemaikaka1@gmail.com>
2025-10-24 16:42:29 +0700

    feat(exercises): update getting-started

---
+++ exercises/getting-started/pull.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+# pull images
+docker pull nginx:1.29-alpine
+docker pull postgres:17-alpine
+docker pull elestio/pgadmin:latest
--- exercises/getting-started/run.sh
+++ exercises/getting-started/run.sh
@@ -1,10 +1,5 @@
 #!/bin/bash
 
-# pull images
-docker pull nginx:1.29-alpine
-docker pull postgres:17-alpine
-docker pull elestio/pgadmin:latest
-
 # run images
 docker run -d --name nginx nginx:1.29-alpine
 
